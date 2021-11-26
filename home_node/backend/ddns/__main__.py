12ac2ea74c4982d15748a9a587131b215947c3b5
BjornRo <54636059+BjornRo@users.noreply.github.com>
2021-11-26 21:06:23 +0100

    updated ddns client to take optional args

--- home_node/backend/ddns/__main__.py
+++ home_node/backend/ddns/__main__.py
@@ -3,7 +3,27 @@ from pathlib import Path
 from time import sleep
 import netifaces
 import requests
+import argparse
 
+parser = argparse.ArgumentParser(description='Settings for time delay')
+parser.add_argument("time", help="Default seconds")
+parser.add_argument("--hour", action="store_true", help="Select hour between")
+parser.add_argument("--min", action="store_true", help="Select min between")
+
+args = parser.parse_args()
+
+SLEEP_TIME = 7200
+
+
+if args.time:
+    v = float(args.time)
+    if 0 < v:
+        if args.hour:
+            SLEEP_TIME = v * 3600
+        elif args.minutes:
+            SLEEP_TIME = v * 60
+        if 60 < SLEEP_TIME:
+            SLEEP_TIME = 7200
 
 def main():
     timeout = 10
@@ -19,7 +39,7 @@ def main():
                 break
         except:
             pass
-        sleep(7200)
+        sleep(SLEEP_TIME)
 
 
 if __name__ == "__main__":
