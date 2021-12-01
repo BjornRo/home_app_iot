7121768447abd287d457792671bf7c0ffac03b8f
BjornRo <54636059+BjornRo@users.noreply.github.com>
2021-12-01 03:28:10 +0100

    script donish

--- home_node/backend/first_run.sh
+++ home_node/backend/first_run.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+set -o allexport; source .env; set +o allexport
+
 PI_ADDRESS='rpis'
 MPATH='/app'
+SERVER=${SERVER_ENV}
+EMAIL=${EMAIL_ENV}
 
 #sudo apt-get update
 #sudo apt-get install certbot python-certbot-nginx
+
+echo "This will delete the entire appdata folder."
+read -p "Press any key to continue..."
+
+# Upload all files
+# Delete and create folders
+rsync -avz \
+--rsync-path="rm -rf ~${MPATH} \
+&& mkdir -p ~${MPATH}/appdata/certbot/letsencrypt \
+&& mkdir -p ~${MPATH}/appdata/db \
+&& rsync" \
+--include=www/build/ \
+--exclude=www/* \
+./ ${PI_ADDRESS}:~${MPATH}
+
+ssh rpis << EOF
+docker run -it --name certbot \
+ -v "./appdata/certbot/letsencrypt:/etc/letsencrypt" \
+ -v "./appdata/certbot/www:/var/www/certbot" \
+ certbot/certbot:latest certonly \
+ -d ${SERVER} --verbose --keep-until-expiring \
+ --agree-tos --email ${EMAIL} \
+ --preferred-challenges=http \
+ --webroot --webroot-path=/var/www/certbot
+EOF
