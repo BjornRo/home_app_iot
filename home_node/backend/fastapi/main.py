ee8ad0a10cec6601a40c2b76b4d105cf34c75159
BjornRo <54636059+BjornRo@users.noreply.github.com>
2021-12-18 18:35:01 +0100

    Added db for users and started working on auth part

--- home_node/backend/fastapi/main.py
+++ home_node/backend/fastapi/main.py
@@ -1,7 +1,10 @@
 from fastapi.middleware.cors import CORSMiddleware
 from importlib import import_module
 from routers import MyRouterAPI
+from databases import Database
+from datetime import datetime
 from fastapi import FastAPI
+from os.path import isfile
 import json
 import os
 
@@ -15,6 +18,39 @@ origins = [  # Internal routing using dnsmasq on my router.
     "http://www.home",
 ]
 
+DB_FILE = "/db/main_app_db.db"
+DB_TABLES = """
+CREATE TABLE users (
+    username VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(100) NOT NULL,
+    access_level VARCHAR(20) NOT NULL,
+    created_date VARCHAR(19) NOT NULL,
+    comment TEXT NOT NULL
+)
+"""
+
+db = Database("sqlite:///" + DB_FILE)
+
+
+@app.on_event("startup")
+async def db_connect():
+    await db.connect()
+
+    # Check if databasefile exists
+    if isfile(DB_FILE):
+        return
+    # Create db file and import tables if db-file doesn't exist
+    await db.execute(query=DB_TABLES)
+    f = open("default_users.json", "r") # desperate for dedenting :(
+    for usr, data in json.load(f).items():
+        data |= {"username": usr, "created_date": datetime.now().isoformat("T", "minutes")}
+        await db.execute("INSERT INTO users VALUES (:username, :password, :access_level, :created_date, :comment)", data)
+    f.close()
+
+
+@app.on_event("shutdown")
+async def db_disconnect():
+    await db.disconnect()
 
 # Load additonal public urls, hiding it at the moment to reduce risk of DoS attack on my own network.
 url: str
--- home_node/backend/fastapi/routers/auth/auth.py
+++ home_node/backend/fastapi/routers/auth/auth.py
@@ -1,8 +1,13 @@
+from fastapi.security import OAuth2PasswordRequestForm
+
 from redis.commands.json import JSON as REJSON_Client
-from fastapi import HTTPException
+
+from fastapi import HTTPException, Depends, status
+
 from typing import Optional, Dict, Union
 from pydantic import BaseModel
 from .. import MyRouterAPI
+from ...main import db
 import bcrypt
 import redis
 
@@ -13,20 +18,14 @@ TAGS = ["auth_api"]
 # To route the routings down the document.
 router = MyRouterAPI(prefix=PREFIX, tags=TAGS).router
 
-
+# Following guide: (Not strictly)
+# https://testdriven.io/blog/developing-a-single-page-app-with-fastapi-and-vuejs/
 @router.post("/login")
-async def login():
-    try:
-        user = ... # TODO get user from db,
-    except:
-        raise HTTPException(401,
-            #status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-        )
-
-    if not bcrypt.checkpw("pass".encode(), b"hash"):
-        raise HTTPException(401,
-            #status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-        )
-    return user
+async def login(payload: OAuth2PasswordRequestForm = Depends()):
+    usrdata = await db.fetch_one("SELECT username, password, access_level FROM users WHERE username = (:username)", {"username": payload.username})
+    if usrdata is None or not bcrypt.checkpw(payload.password.encode(), usrdata[1].encode()):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password",
+                            headers={"WWW-Authenticate": "Bearer"}
+                            )
+    return {"username": payload.username, "access_level": usrdata[2]}
