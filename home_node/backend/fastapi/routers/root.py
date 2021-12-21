2b94e8221ae59990db9fc63227bc4b9019f214fa
BjornRo <54636059+BjornRo@users.noreply.github.com>
2021-12-22 00:25:22 +0100

    added a hardcoded path to fastapi

--- home_node/backend/fastapi/routers/root.py
+++ home_node/backend/fastapi/routers/root.py
@@ -9,10 +9,68 @@ TAGS = ["root"]
 router = MyRouterAPI(prefix=PREFIX, tags=TAGS).router
 
 # Routing
+
+
 @router.get("/", include_in_schema=False)
 async def root():
-    return {"hello":"world"}
+    return {"hello": "world"}
+
 
 @router.get('/favicon.ico', include_in_schema=False)
 async def favicon():
     return FileResponse("static/favicon.ico")
+
+
+@router.get('/balc_status', include_in_schema=False)
+async def balc_status():
+    return {"status": [0, 0, 1, 0]}
+
+
+@router.get('/sensor_resp', include_in_schema=False)
+async def sensor_resp():
+    return {
+        "home": {
+            "balcony": {
+                "time": "2021-12-31T13:37:59.12345",
+                "new": True,
+                "data": {
+                    "temperature": 42,
+                    "humidity": 33.4,
+                }
+            },
+            "bikeroom": {
+                "time": "2021-12-31T00:13:37.12345",
+                "new": True,
+                "data": {
+                    "temperature": -42,
+                }
+            },
+            "kitchen": {
+                "time": "2021-12-31T11:13:37.12345",
+                "new": True,
+                "data": {
+                    "temperature": -42,
+                    "humidity": 99.9,
+                    "airpressure": 1024.64
+                }
+            },
+        },
+        "remote_sh": {
+            "pizw": {
+                "time": "2021-12-31T13:37:59.12345",
+                "new": True,
+                "data": {
+                    "temperature": 42,
+                }
+            },
+            "hydrofor": {
+                "time": "2021-12-31T00:13:37.12345",
+                "new": True,
+                "data": {
+                    "temperature": -42,
+                    "humidity": 99.9,
+                    "airpressure": 999.9
+                }
+            }
+        }
+    }
